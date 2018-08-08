4bdc791691217cf0f1341fb8682eb7ceeb5cb733
matiasjgoitea <matiasjgoitea@gmail.com>
2018-08-08 15:21:28 -0300

    Initial boilerplate running with #create-react-app and skeleton folders

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import CanvasInvaders from './canvas/CanvasInvaders.js'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <CanvasInvaders />
+    )
+  }
+}
---
+++ src/components/canvas/CanvasInvaders.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './CanvasInvaders.css'
+
+export default class CanvasInvaders extends React.Component {
+  render() {
+    return (
+      <h1> ¡Destroy all humans! </h1>
+    )
+  }
+}
